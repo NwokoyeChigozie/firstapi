b87fcadbb93d871ceb242d67469df257ce8de4fd
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2021-08-20 16:41:44 +0100

    add file

--- views/contact.go
+++ views/contact.go
@@ -11,5 +11,5 @@ func Contact(response http.ResponseWriter, request *http.Request) {
 	var contact models.Contact
 	response.Header().Set("Content-Type", "application/json")
 	_ = json.NewDecoder(request.Body).Decode(&contact)
-	json.NewEncoder(response).Encode(contact)
+	json.NewEncoder(response).Encode(response)
 }
